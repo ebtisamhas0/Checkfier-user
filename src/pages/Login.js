5f396647d05884c2752a16dfa5267ea672389409
ebtisamhas0 <ebtisamalqarni08@gmail.com>
2023-04-05 08:07:14 +0300

    update app.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ export function Login() {
         setError(null);
         try {
             
-          const response = await fetch('http://localhost:3000/login', {
+          const response = await fetch('/login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ phone }),
