25306d798f6332ebe2807456032aa0b8b8ddba7a
ebtisamhas0 <ebtisamalqarni08@gmail.com>
2023-05-28 02:04:09 +0300

    Last Edit

--- server.js
+++ server.js
@@ -378,7 +378,7 @@ app.get('/notifications', async (req, res) => {
 
 
  
-app.get('/signup', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
